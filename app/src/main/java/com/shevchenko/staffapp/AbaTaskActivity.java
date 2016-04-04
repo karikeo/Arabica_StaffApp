adb4a0b2759c4f1efcdc7d0a11d2cd8a777cdc1c
Olga Mezhina <olga.mezhina@gmail.com>
2016-04-04 22:54:26 +0300

    let user enable bt

--- app/src/main/java/com/shevchenko/staffapp/AbaTaskActivity.java
+++ app/src/main/java/com/shevchenko/staffapp/AbaTaskActivity.java
@@ -246,6 +246,10 @@ public class AbaTaskActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CaptureViewHolder.BT_REQUEST_CODE) {
+            captureViewHolder.start();
+            return;
+        }
         if (resultCode != RESULT_OK) {
             return;
         }
